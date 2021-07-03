cba5f5a652a74de07fc95ef31a54a14283201a7b
AkshatRawat18 <akshatrawat555@gmail.com>
2021-07-03 19:37:20 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-
-import React,{Component} from "react";
-class App extends Component{
-render() {
-  return (
-    <div>
-      Phone Directory
-    </div>
-  );
-}
+import React, { Component } from "react";
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {/* <div className="header">Phone Directory</div>
+        <button> Add</button>
+        <div>
+          <span> Name </span>
+          <br></br>
+          <span> Phone </span>
+        </div> */}
+        <label htmlFor="name">Name: </label>
+        <input id="name" type="text" placeholder="Type Here" defaultValue="Akshat"/>
+      </div>
+    );
+  }
 }
 
 export default App;
